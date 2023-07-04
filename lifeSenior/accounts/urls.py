0ff1cf74a6f3402ec00a50a66ee8f4224d0347d8
이현승 <77336664+gesal03@users.noreply.github.com>
2023-07-05 00:15:56 +0900

    add default value

--- lifeSenior/accounts/urls.py
+++ lifeSenior/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, signUp
 
 app_name='accounts'
 
 urlpatterns = [
     path('', home),
+    path('signUp', signUp, name='signUp'),
 ]
     
--- lifeSenior/accounts/views.py
+++ lifeSenior/accounts/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from .models import Profile
 
 # Create your views here.
 def home(request):
+    return render(request, 'main.html')
+
+def signUp(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password1']
+            )
+            profile = Profile(user=user)
+            profile.save
+            auth.login(request,user)
+            return redirect('/')
+        return redirect('accounts:signUp')
     return render(request, 'main.html')
