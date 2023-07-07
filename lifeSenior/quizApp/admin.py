9af8474cdba2afcc2e9ebc76d174d61c3e958936
이현승 <77336664+gesal03@users.noreply.github.com>
2023-07-07 17:59:44 +0900

    add main view

--- lifeSenior/quizApp/admin.py
+++ lifeSenior/quizApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Quiz, Choice
 # Register your models here.
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('text', 'description', 'category', 'difficulty')
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('quiz', 'text')
+
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Choice, ChoiceAdmin)
