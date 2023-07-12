b5de5a56e2e4d252e488d672c4f99ac4a94288b0
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-07-12 15:25:20 +0900

    studyJs.js 수정
    항목별 점수 그래프 안보이는 이슈 수정

--- lifeSenior/static/js/studyJS.js
+++ lifeSenior/static/js/studyJS.js
@@ -332,28 +332,32 @@ window.onload = function() {
 const allScoreDataValues = [2, 4, 3, 2, 3, 6, 0]; //맞춘 수
 const allScoreGraphViews = document.querySelectorAll('#all-score-body');
 
-// 데이터 값에 따라 높이를 조정
-allScoreGraphViews.forEach(function(allScoreGraphView, index) {
-  const allScoreDataValue = allScoreDataValues[index]; // 해당 요소에 대응하는 데이터 값
-  allScoreGraphView.style.height = `${(638 / 6) * allScoreDataValue}px`; // 데이터 값을 픽셀 단위로 높이로 설정
-});
-//마지막 그래프 색깔 변경
-const allScoreLastScoreGraphView = allScoreGraphViews[allScoreGraphViews.length - 1];
-allScoreLastScoreGraphView.style.backgroundColor = '#FF5B16';
+if (allScoreGraphViews.length > 0) {
+  // 데이터 값에 따라 높이를 조정
+  allScoreGraphViews.forEach(function(allScoreGraphView, index) {
+    const allScoreDataValue = allScoreDataValues[index]; // 해당 요소에 대응하는 데이터 값
+    allScoreGraphView.style.height = `${(638 / 6) * allScoreDataValue}px`; // 데이터 값을 픽셀 단위로 높이로 설정
+  });
+  //마지막 그래프 색깔 변경
+  const allScoreLastScoreGraphView = allScoreGraphViews[allScoreGraphViews.length - 1];
+  allScoreLastScoreGraphView.style.backgroundColor = '#FF5B16';
+}
 
 
 //항목별 점수 그래프 높이 설정
 const notAllScoreDataValues = [2, 4, 5, 2, 3, 2, 4]; //맞춘 수
 const notAllScoreGraphViews = document.querySelectorAll('#not-all-score-body');
 
-// 데이터 값에 따라 높이를 조정
-notAllScoreGraphViews.forEach(function(notAllScoreGraphView, index) {
-  const notAllScoreDataValue = notAllScoreDataValues[index]; // 해당 요소에 대응하는 데이터 값
-  notAllScoreGraphView.style.height = `${(638 / 5) * notAllScoreDataValue}px`; // 데이터 값을 픽셀 단위로 높이로 설정
-});
-//마지막 그래프 색깔 변경
-const notAllScoreLastScoreGraphView = notAllScoreGraphViews[notAllScoreGraphViews.length - 1];
-notAllScoreLastScoreGraphView .style.backgroundColor = '#FF5B16';
+if (notAllScoreDataValues.length > 0) {
+  // 데이터 값에 따라 높이를 조정
+  notAllScoreGraphViews.forEach(function(notAllScoreGraphView, index) {
+    const notAllScoreDataValue = notAllScoreDataValues[index]; // 해당 요소에 대응하는 데이터 값
+    notAllScoreGraphView.style.height = `${(638 / 5) * notAllScoreDataValue}px`; // 데이터 값을 픽셀 단위로 높이로 설정
+  });
+  //마지막 그래프 색깔 변경
+  const notAllScoreLastScoreGraphView = notAllScoreGraphViews[notAllScoreGraphViews.length - 1];
+  notAllScoreLastScoreGraphView.style.backgroundColor = '#FF5B16';
+}
 
 
 // //그래프 하나만 보이게
