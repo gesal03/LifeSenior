b605b8bcb8dbf531c798fb3fbc19959248a8055a
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-07-14 04:54:59 +0900

    main 선택 분류 배열

--- lifeSenior/static/js/mainJs.js
+++ lifeSenior/static/js/mainJs.js
@@ -1,37 +1,35 @@
-//conmunity-btn 클릭한 요소에만 색깔 변경
-const container = document.querySelector('.container-community-btn');
-const submitButtons = container.querySelectorAll('input[type="submit"]');
+// //conmunity-btn 클릭한 요소에만 색깔 변경
+// const container = document.querySelector('.container-community-btn');
+// const submitButtons = container.querySelectorAll('input[type="submit"]');
 
+// // submitButtons.forEach(function(button) {
+// //   button.addEventListener('click', function() {
+// //     submitButtons.forEach(function(btn) {
+// //       if (btn !== button) {
+// //         btn.classList.remove('active');
+// //       }
+// //     });
+// //     button.classList.toggle('active');
+// //   });
+// // });
 // submitButtons.forEach(function(button) {
 //   button.addEventListener('click', function() {
 //     submitButtons.forEach(function(btn) {
-//       if (btn !== button) {
-//         btn.classList.remove('active');
-//       }
+//       // btn.classList.remove('active');
 //     });
-//     button.classList.toggle('active');
+//     button.classList.add('active');
 //   });
 // });
-submitButtons.forEach(function(button) {
-  button.addEventListener('click', function() {
-    submitButtons.forEach(function(btn) {
-      // btn.classList.remove('active');
-    });
-    button.classList.add('active');
-  });
-});
 
+// document.addEventListener('click', function(event) {
+//   const target = event.target;
 
-submitButtons[0].classList.add('active');
-document.addEventListener('click', function(event) {
-  const target = event.target;
-
-  if (!target.matches('input[type="submit"]') && !container.contains(target)) {
-    submitButtons.forEach(function(button) {
-      button.classList.remove('active');
-    });
-  }
-});
+//   if (!target.matches('input[type="submit"]') && !container.contains(target)) {
+//     submitButtons.forEach(function(button) {
+//       button.classList.remove('active');
+//     });
+//   }
+// });
 
 $(".quiz-correct-modal").hide();
 $(".quiz-incorrect-modal").hide();
