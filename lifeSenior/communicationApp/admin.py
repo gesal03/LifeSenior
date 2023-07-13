7a33fe57bf26bf4085c59be7769794ecc43314b8
drgnseok <dydtjr410@naver.com>
2023-07-13 19:57:34 +0900

    Add communicationApp admin.py

--- lifeSenior/communicationApp/admin.py
+++ lifeSenior/communicationApp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Question, Answer, Comment
 
 # Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'date', 'recommend', 'views')
+
+admin.site.register(Question, QuestionAdmin)
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ()
+
+admin.site.register(Answer, AnswerAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ()
+
+admin.site.register(Comment, CommentAdmin)
--- lifeSenior/communicationApp/models.py
+++ lifeSenior/communicationApp/models.py
@@ -6,27 +6,27 @@ user = get_user_model()
 
 #질문
 class Question(models.Model):
-    autor = models.ForeignKey(user, on_delete=models.CASCADE, related_name='author_question') #작성자
-    title = models.CharField("TITLE", max_length=20, default="질문 제목") #질문 제목
-    content = models.TextField("CONTENT", max_length=50, default="질문 내용") #질문 내용
-    date = models.DateTimeField("DATE", auto_now_add=True) #작성일자
-    recommend = models.PositiveIntegerField("RECOMMEND", default=0) #추천수
-    category = models.CharField("CATEGORY", max_length=10) #카테고리
-    views = models.PositiveIntegerField("VIEWS", default=0) #조회수
-    answerd = models.BooleanField("ANSWERD", default=False) #답변 여부
+    autor       = models.ForeignKey(user, on_delete=models.CASCADE, related_name='author_question') #작성자
+    title       = models.CharField("TITLE", max_length=20, default="질문 제목") #질문 제목
+    content     = models.TextField("CONTENT", max_length=300, default="질문 내용") #질문 내용
+    date        = models.DateTimeField("DATE", auto_now_add=True) #작성일자
+    recommend   = models.PositiveIntegerField("RECOMMEND", default=0) #추천수
+    category    = models.CharField("CATEGORY", max_length=10) #카테고리
+    views       = models.PositiveIntegerField("VIEWS", default=0) #조회수
+    answerd     = models.BooleanField("ANSWERD", default=False) #답변 여부
     
 
 #질문에 대한 답변
 class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE) #questionFK
-    autor = models.ForeignKey(user, on_delete=models.CASCADE, related_name='author_answer') #작성자
-    content = models.TextField("CONTENT", max_length=50, default="답변 내용") #답변 내용
-    image = models.ImageField("IMAGE", upload_to="questionAnswer/", default="static/img/defaultImg.png") #답변 사진
-    recommend = models.ManyToManyField(user, related_name='voer_question') #추천 수 수집을 위한 모델
+    question    = models.ForeignKey(Question, on_delete=models.CASCADE) #questionFK
+    autor       = models.ForeignKey(user, on_delete=models.CASCADE, related_name='author_answer') #작성자
+    content     = models.TextField("CONTENT", max_length=50, default="답변 내용") #답변 내용
+    image       = models.ImageField("IMAGE", upload_to="questionAnswer/", default="static/img/defaultImg.png") #답변 사진
+    recommend   = models.ManyToManyField(user, related_name='voer_question') #추천 수 수집을 위한 모델
 
 #댓글
 class Comment(models.Model):
-    autor = models.ForeignKey(user, on_delete=models.CASCADE) #작성자
-    question = models.ForeignKey(Question, on_delete=models.CASCADE) #질문
-    content = models.CharField("CONTENT", max_length=20, default="댓글입력") #댓글 내용
-    date = models.DateField("DATE", auto_now_add=True) #작성 날짜
+    autor       = models.ForeignKey(user, on_delete=models.CASCADE) #작성자
+    question    = models.ForeignKey(Question, on_delete=models.CASCADE) #질문
+    content     = models.CharField("CONTENT", max_length=20, default="댓글입력") #댓글 내용
+    date        = models.DateField("DATE", auto_now_add=True) #작성 날짜
