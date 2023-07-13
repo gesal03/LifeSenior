1012dd9b2f52346e76ea74486133ba587df501b5
drgnseok <dydtjr410@naver.com>
2023-07-14 02:19:38 +0900

    question_create add

--- lifeSenior/communicationApp/views.py
+++ lifeSenior/communicationApp/views.py
@@ -10,7 +10,7 @@ def home(request):
 def communication_list(request):
     communication_list = Question.objects.all().order_by('-date')
     context = {
-        'communication_list': communication_list
+        'communication_list': communication_list,
     }
     return render(request, 'communicationAPP/communication_list.html', context)
 
@@ -46,7 +46,16 @@ def my_answer(request):
 
 #질문하기 작성할 때 : question_create
 def question_create(request):
-    return render(request, 'communicationAPP/question_create.html')
+    if request.method == 'POST':
+        question = Question(autor=request.user, 
+                            title=request.POST['title'],
+                            category=request.POST['category'], 
+                            content=request.POST['content']
+                            )
+        question.save()
+        return redirect('communivationApp:communcation_list')
+    else:
+        return render(request, 'communicationAPP/question_create.html')
 
 #답변하기 작성할 때 : answer_create
 def answer_create(request):
