bda38a970b5b8d1f591b7a2461ea1e5e8ff4b892
drgnseok <dydtjr410@naver.com>
2023-07-13 18:46:56 +0900

    add templates language

--- lifeSenior/communicationApp/views.py
+++ lifeSenior/communicationApp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import PostForm
-from .models import Post
+from .models import Question
 
 # Create your views here.
 def home(request):
@@ -9,8 +8,23 @@ def home(request):
 #-----기본 메인 화면-----
 #소통게시판 : communication_list
 def communication_list(request):
-    posts = Post.objects.all()
-    return render(request, 'communicationApp/communication_list.html', {'posts': posts})
+    communication_list = Question.objects.all().order_by('-date')
+    context = {
+        'communication_list': communication_list
+    }
+    return render(request, 'communicationAPP/communication_list.html', context)
+
+#소통게시판 메인_질문하나 클릭하면 나오는 화면 : communication_detail
+def communication_detail(request):
+    return render(request, 'communicationAPP/communication_detail.html')
+
+#답변하기 : answer_list
+def answer_list(request):
+    return render(request, 'communicationAPP/answer_list.html')
+
+#질문에 답변하기_질문하나 클릭시 : answer_detail
+def answer_detail(request, answer_detail):
+    return render(request, 'communicationAPP/answer_detail.html')
 
 #내가 한 질문 : my_question
 def my_question(request):
@@ -18,40 +32,17 @@ def my_question(request):
 
 #내가 한 답변 : my_answer
 def my_answer(request):
-    return render(request, 'my_answer.html')
+    return render(request, 'communicationAPP/my_answer.html')
 
 #질문하기 작성할 때 : question_create
 def question_create(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('communicationApp:communication_list')
-    else:
-        form = PostForm()
-    return render(request, 'communicationApp/question_create.html', {'form': form})
+    return render(request, 'communicationAPP/question_create.html')
 
 #답변하기 작성할 때 : answer_create
 def answer_create(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('communicationApp:communication_list')
-    else:
-        form = PostForm()
-    return render(request, 'communicationApp/answer_create.html', {'form': form})
-
-#답변하기 : answer_list
-def answer_list(request):
-    return render(request, 'answer_list.html')
+    return render(request, 'communicationAPP/answer_create.html')
 
 #답변하기 추천 기능 : answer_recommend
-@login_required
 def answer_recommend(request, answer_id):
-    answer = get_object_or_404(Answer, pk=answer_id)
-    if request.user == answer.author:
-        message.error(request, '본인이 작성한 글은 추천할 수 없습니다.')
-    else:
-        answer.recommend.add(request.user)
-    return redirect('communicationApp:answer_list', question_id=answer.question.id)
+    return render(request, 'answer_recommend.html')
+
